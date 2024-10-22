234b0bbbe26a20cd9f718c7be12c8e89565388c6
Zahharius2 <148521669+Zahharius2@users.noreply.github.com>
2024-10-22 12:34:15 +0300

    Add files via upload

---
+++ iceandfire/precache-manifest.041082217547a3c83aa633c3858e663c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9dd543cd75ab0e462821",
+    "url": "/zarabotai.github.io/iceandfire/index.html/static/css/main.f0665610.chunk.css"
+  },
+  {
+    "revision": "9dd543cd75ab0e462821",
+    "url": "/zarabotai.github.io/iceandfire/index.html/static/js/main.9dd543cd.chunk.js"
+  },
+  {
+    "revision": "6260e76fc4f32b676b1d",
+    "url": "/zarabotai.github.io/iceandfire/index.html/static/css/1.4f5e3e7b.chunk.css"
+  },
+  {
+    "revision": "6260e76fc4f32b676b1d",
+    "url": "/zarabotai.github.io/iceandfire/index.html/static/js/1.6260e76f.chunk.js"
+  },
+  {
+    "revision": "b9bcadc82043ad4bbabb",
+    "url": "/zarabotai.github.io/iceandfire/index.html/static/js/runtime~main.b9bcadc8.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/zarabotai.github.io/iceandfire/index.html/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/zarabotai.github.io/iceandfire/index.html/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "28c3891067d9d9fe0a7348ebfddd1e04",
+    "url": "/zarabotai.github.io/iceandfire/index.html/index.html"
+  }
+];
