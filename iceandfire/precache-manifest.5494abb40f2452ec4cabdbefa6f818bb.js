8c4c36f5ecda581f4f89ed87d18302be71c8b4ea
Zahharius2 <148521669+Zahharius2@users.noreply.github.com>
2024-10-22 12:45:26 +0300

    Add files via upload

---
+++ iceandfire/precache-manifest.5494abb40f2452ec4cabdbefa6f818bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2166493d99ab10a4ecae",
+    "url": "/iceandfire/index.html/static/css/main.8a8bc470.chunk.css"
+  },
+  {
+    "revision": "2166493d99ab10a4ecae",
+    "url": "/iceandfire/index.html/static/js/main.2166493d.chunk.js"
+  },
+  {
+    "revision": "6260e76fc4f32b676b1d",
+    "url": "/iceandfire/index.html/static/css/1.4f5e3e7b.chunk.css"
+  },
+  {
+    "revision": "6260e76fc4f32b676b1d",
+    "url": "/iceandfire/index.html/static/js/1.6260e76f.chunk.js"
+  },
+  {
+    "revision": "75414755bf1c3bb4ddac",
+    "url": "/iceandfire/index.html/static/js/runtime~main.75414755.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/iceandfire/index.html/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/iceandfire/index.html/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "1982c992fcacf8128a7bf1b66b5a9f6c",
+    "url": "/iceandfire/index.html/index.html"
+  }
+];
